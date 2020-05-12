9d7ea67578940cb284847c2db410328147129b6c
Zachary Henderson <zachary.henderson114@gmail.com>
2020-05-12 09:19:32 -0500

    img with pug, added public folder, fixed style folder naming, created views folder, added path dependancy, added icon

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
