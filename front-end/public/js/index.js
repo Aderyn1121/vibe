a05f335884755be4cbf8b7a9e7731b13d6bf75de
Zachary Henderson <zachary.henderson114@gmail.com>
2020-05-14 22:35:38 -0500

    Style (#10)
    * optimized how the CSS loads, updated bullet points for frontend in readme
    
    * started login page, need to fix button issue

--- front-end/public/js/index.js
+++ front-end/public/js/index.js
@@ -1,12 +1,3 @@
-const signUpButton = document.getElementsByClassName('nav-bar__sign-up');
-
-signUpButton[0].addEventListener('click', async (e) => {
-  const res = await fetch('http://localhost:8081/signup/ajax');
-  const data = await res.json();
-
-  document = data;
-});
-
 // button1.addEventListener('click', async (e) => {
 //   const res = await fetch('http://localhost:8081/body1/button');
 //   const data = await res.json();
