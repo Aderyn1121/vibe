27d3e5ad5e08b1db1b9d23259c1411beac00afec
Zachary Henderson <zachary.henderson114@gmail.com>
2020-05-18 11:46:20 -0500

    changed font, added songs to seed files

--- back-end/db/seeders/20200517081711-add-test-data.js
+++ back-end/db/seeders/20200517081711-add-test-data.js
@@ -1,117 +1,227 @@
 'use strict';
-const faker = require("faker");
-const bcrypt = require("bcryptjs");
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.bulkInsert('Users', [
-      { 
-        email: 'john@doe.com',
-        hashedPassword: bcrypt.hashSync(faker.internet.password()), 
-        nickName: 'John Doe', birthday: new Date('01-01-2020'), 
-        createdAt: new Date(), 
-        updatedAt: new Date () 
-      },
-      { 
-        email: 'jane@doe.com',
-        hashedPassword: bcrypt.hashSync(faker.internet.password()), 
-        nickName: 'Jane Doe', birthday: new Date('01-01-2020'), 
-        createdAt: new Date(), 
-        updatedAt: new Date () 
-      },
-    ], { returning: true });
+    const users = await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'john@doe.com',
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          nickName: 'John Doe',
+          birthday: new Date('01-01-2020'),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          email: 'jane@doe.com',
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          nickName: 'Jane Doe',
+          birthday: new Date('01-01-2020'),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
 
-    const playlists = await queryInterface.bulkInsert('Playlists', [
-      { 
-        playlistName: 'My Jamz', 
-        userId: users[0].id, 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-      },
-      { 
-        playlistName: 'Weekend Vibes', 
-        userId: users[0].id, 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-      },
-      { 
-        playlistName: 'Chillin', 
-        userId: users[1].id, 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-      },
-    ], {returning: true });
+    const playlists = await queryInterface.bulkInsert(
+      'Playlists',
+      [
+        {
+          playlistName: 'My Jamz',
+          userId: users[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          playlistName: 'Weekend Vibes',
+          userId: users[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          playlistName: 'Chillin',
+          userId: users[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          playlistName: 'I love music',
+          userId: users[1].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
 
-    const artists = await queryInterface.bulkInsert('Artists', [
-      {
-        artistName: 'Queen', 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-      },
-      {
-        artistName: 'The Weeknd', 
-        createdAt: new Date(), 
-        updatedAt: new Date()
-      },
-    ], {returning: true });
-    
-    const albums = await queryInterface.bulkInsert('Albums', [
-      { 
-        albumName: 'Jazz', 
-        releaseDate: new Date('01-05-1979'), 
-        artistId: artists[0].id, 
-        createdAt: new Date(), 
-        updatedAt: new Date() 
-      },
-      { 
-        albumName: 'The Weeknd XO', 
-        releaseDate: new Date('03-19-2020'), 
-        artistId: artists[1].id, 
-        createdAt: new Date(), 
-        updatedAt: new Date() 
-      },
-    ], {returning: true });
-    
-    const songs = await queryInterface.bulkInsert('Songs', [
-      { 
-        songName: "Don't stop me now", 
-        releaseDate: albums[0].releaseDate,
-        albumId: albums[0].id,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      { 
-        songName: "Escape From LA", 
-        releaseDate: albums[0].releaseDate,
-        albumId: albums[1].id,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ], { returning: true });
+    const artists = await queryInterface.bulkInsert(
+      'Artists',
+      [
+        {
+          artistName: 'Queen',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          artistName: 'The Weeknd',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          artistName: 'Imogen Heap',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          artistName: 'Type (A) Alert',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          artistName: 'Groove for Thought',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
 
-    return queryInterface.bulkInsert('PlaylistSongs', [
-      {
-        song: songs[0].songName,
-        playlistId: playlists[0].id,
-        songId: songs[0].id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        song: songs[1].songName,
-        playlistId: playlists[0].id,
-        songId: songs[1].id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        song: songs[1].songName,
-        playlistId: playlists[1].id,
-        songId: songs[1].id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-  ], {});
+    const albums = await queryInterface.bulkInsert(
+      'Albums',
+      [
+        {
+          albumName: 'Jazz',
+          releaseDate: new Date('01-05-1979'),
+          artistId: artists[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumName: 'The Weeknd XO',
+          releaseDate: new Date('03-19-2020'),
+          artistId: artists[1].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumName: 'Speak for Yourself',
+          releaseDate: new Date('07-18-2005'),
+          artistId: artists[2].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumName: 'Seasons- Single',
+          releaseDate: new Date('07-18-2005'),
+          artistId: artists[3].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          albumName: 'Unnecessary Dissonance',
+          releaseDate: new Date('12-1-2014'),
+          artistId: artists[4].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
+
+    const songs = await queryInterface.bulkInsert(
+      'Songs',
+      [
+        {
+          songName: "Don't stop me now",
+          releaseDate: albums[0].releaseDate,
+          albumId: albums[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          songName: 'Escape From LA',
+          releaseDate: albums[1].releaseDate,
+          albumId: albums[1].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          songName: 'Hide and Seek',
+          releaseDate: albums[2].releaseDate,
+          albumId: albums[2].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          songName: 'Seasons',
+          releaseDate: albums[3].releaseDate,
+          albumId: albums[3].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          songName: 'Teach Me Tonight',
+          releaseDate: albums[4].releaseDate,
+          albumId: albums[4].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
+
+    return queryInterface.bulkInsert(
+      'PlaylistSongs',
+      [
+        {
+          song: songs[0].songName,
+          playlistId: playlists[0].id,
+          songId: songs[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          song: songs[1].songName,
+          playlistId: playlists[0].id,
+          songId: songs[1].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          song: songs[1].songName,
+          playlistId: playlists[1].id,
+          songId: songs[1].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          song: songs[2].songName,
+          playlistId: playlists[3].id,
+          songId: songs[2].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          song: songs[3].songName,
+          playlistId: playlists[3].id,
+          songId: songs[3].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          song: songs[4].songName,
+          playlistId: playlists[3].id,
+          songId: songs[4].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -121,5 +231,5 @@ module.exports = {
     await queryInterface.bulkDelete('Artists', null, {});
     await queryInterface.bulkDelete('Playlists', null, {});
     return queryInterface.bulkDelete('Users', null, {});
-  }
+  },
 };
