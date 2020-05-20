cd3f52127eafa78eb7382a0da5541b589bc85745
Zachary Henderson <zachary.henderson114@gmail.com>
2020-05-19 22:16:37 -0500

    Back end (#57)
    * fixed song id issue
    
    * are you serious??
    
    Co-authored-by: gootieno <geoff.otieno7@gmail.com>

--- front-end/app.js
+++ front-end/app.js
@@ -38,14 +38,6 @@ app.get('/signup', (req, res) => {
   });
 });
 
-app.get('/login', (req, res) => {
-  res.render('login', {
-    fileName: 'login',
-    title: 'Login',
-    scripts: ['login'],
-  });
-});
-
 // Error handling stuff
 
 app.use((req, res, next) => {
