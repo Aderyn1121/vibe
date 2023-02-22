a1663a548b1f1b848b6ea6ce317e357d19b72cbc
gootieno <geoff.otieno7@gmail.com>
2023-02-21 20:55:39 -0800

    added db url

--- config/database.js
+++ config/database.js
@@ -1,17 +1,23 @@
-const { username, password, database, host } = require('./index').db;
+const { username, password, database, host } = require("./index").db;
 
 module.exports = {
-    development: {
-        username,
-        password,
-        database,
-        host,
-        dialect: 'postgres',
-        seederStorage: 'sequelize',
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  production: {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION
+      },
     },
-    production: {
-        dialect: 'postgres',
-        seederStorage: 'sequelize',
-        use_env_variable: 'DATABASE_URL',
-    },
+    seederStorage: "sequelize",
+    use_env_variable: "DATABASE_URL",
+  },
 };
