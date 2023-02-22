d3ff1bb7625bc079705b65594d9aae522dccdf46
gootieno <geoff.otieno7@gmail.com>
2023-02-21 20:26:37 -0800

    fixing deployment

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const apiRouter = require('./routes/backend-routes/index');
-const viewRouter = require('./routes/frontend-routes/index');
-const { environment } = require('./config');
-const isProduction = environment === 'production';
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const apiRouter = require("./routes/backend-routes/index");
+const viewRouter = require("./routes/frontend-routes/index");
+const { environment } = require("./config");
+const isProduction = environment === "production";
+// require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -13,30 +14,30 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Frontend
-app.set('view engine', 'pug');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/', viewRouter);
+app.set("view engine", "pug");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/", viewRouter);
 
 // Backend
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.status = 404;
-    err.errors = ['Could not find string of resource'];
-    next(err);
+  const err = new Error("The requested resource couldn't be found.");
+  err.status = 404;
+  err.errors = ["Could not find string of resource"];
+  next(err);
 });
 
 // Generic error handler.
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        title: err.title || 'Server Error',
-        message: err.message,
-        errors: err.errors,
-        stack: isProduction ? null : err.stack,
-    });
+  res.status(err.status || 500);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
 });
 
 module.exports = app;
